b1a08470b5a15c7c83a0acb13daef16ea1a6e3fb
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-11-13 22:57:40 -0500

    Launch website version 1.0.0

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,57 +1,13 @@
-import { useState, useEffect } from 'react' 
-
 import * as Page from '../components/Page'
-import * as Navbar from '../components/Navbar'
-import * as Header from '../components/Header'
 
-import * as Logo from '../components/Logo'
-import * as Text from '../components/Text'
+import GlobalNavbar from '../components/sections/GlobalNavbar'
+import IndexHeader from '../components/sections/IndexHeader'
+import DailySpecials from '../components/sections/DailySpecials'
+import OurStory from '../components/sections/OurStory'
+import ContactForm from '../components/sections/ContactForm'
+import GlobalFooter from '../components/sections/GlobalFooter'
 
-import Container from '../components/Container'
-
-import headerImage from '../public/images/contact-header.jpg'
-
-export default function ComingSoonPage() {
-  const calculateTimeLeft = () => {
-    let year = new Date().getFullYear()
-    const difference = +new Date(`11/14/${year}`) - +new Date()
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60)
-      }
-    }
-
-    return timeLeft
-  }
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft())
-    }, 1000)
-    return () => clearTimeout(timer)
-  })
-
-  const timerComponents = []
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return
-    }
-  
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    )
-  })
-
+export default function IndexPage() {
   return (
     <Page.Wrapper
       slug="/"
@@ -59,50 +15,20 @@ export default function ComingSoonPage() {
       img="/images/og.jpg"
       className="IndexPage"
     >
-      {/* <header>
-        <Navbar.OverlayWrapper>
-          <Navbar.Logo>
-            <Logo.Animated color="255, 255, 255" id="navbar" /> 
-          </Navbar.Logo>
-        </Navbar.OverlayWrapper>
-      </header> */}
+      <header>
+        <GlobalNavbar />
+      </header>
       
       <main>
-        <Header.IndexWrapper>
-          <Header.Background
-            href={headerImage}
-          />
-
-          <Header.Overlay>
-            <div
-              style={{ position: 'relative', height: '100%', padding: '4rem 0 2rem 0' }}
-            >
-              <div style={{ position: 'relative', top: '50%', transform: 'translateY(-50%)' }}>
-                <Container>
-                  <div>
-                    <Text.Heading1>
-                      {timerComponents.length ? "Coming Soon" : "We're going live shortly!" }
-                    </Text.Heading1>
-                    <Text.SHeading>
-                      {timerComponents.length ? timerComponents : "" }
-                    </Text.SHeading>
-                  </div>
-                  <br />
-                  <Text.SHeading>
-                    {"For any questions, feel free to contact us at "}
-                    <Text.Anchor
-                      href="mailto:mammaritascucina@gmail.com"
-                    >
-                      mammaritascucina@gmail.com
-                    </Text.Anchor>
-                    .
-                  </Text.SHeading>
-                </Container>
-              </div>
-            </div>
-          </Header.Overlay>
-        </Header.IndexWrapper>
+        <IndexHeader />
+        <DailySpecials showButton />
+        <OurStory />
+        <ContactForm />
       </main>
+
+      <footer>
+        <GlobalFooter />
+      </footer>
     </Page.Wrapper>
   )
 }
---
+++ pages/soon.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react' 
+
+import * as Page from '../components/Page'
+import * as Navbar from '../components/Navbar'
+import * as Header from '../components/Header'
+
+import * as Logo from '../components/Logo'
+import * as Text from '../components/Text'
+
+import Container from '../components/Container'
+
+import headerImage from '../public/images/contact-header.jpg'
+
+export default function ComingSoonPage() {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear()
+    const difference = +new Date(`11/14/${year}`) - +new Date()
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      }
+    }
+
+    return timeLeft
+  }
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+    return () => clearTimeout(timer)
+  })
+
+  const timerComponents = []
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return
+    }
+  
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    )
+  })
+
+  return (
+    <Page.Wrapper
+      slug="/"
+      description="We make food just like nonna, mouthwatering and always delicious."
+      img="/images/og.jpg"
+      className="IndexPage"
+    >
+      {/* <header>
+        <Navbar.OverlayWrapper>
+          <Navbar.Logo>
+            <Logo.Animated color="255, 255, 255" id="navbar" /> 
+          </Navbar.Logo>
+        </Navbar.OverlayWrapper>
+      </header> */}
+      
+      <main>
+        <Header.IndexWrapper>
+          <Header.Background
+            href={headerImage}
+          />
+
+          <Header.Overlay>
+            <div
+              style={{ position: 'relative', height: '100%', padding: '4rem 0 2rem 0' }}
+            >
+              <div style={{ position: 'relative', top: '50%', transform: 'translateY(-50%)' }}>
+                <Container>
+                  <div>
+                    <Text.Heading1>
+                      {timerComponents.length ? "Coming Soon" : "We're going live shortly!" }
+                    </Text.Heading1>
+                    <Text.SHeading>
+                      {timerComponents.length ? timerComponents : "" }
+                    </Text.SHeading>
+                  </div>
+                  <br />
+                  <Text.SHeading>
+                    {"For any questions, feel free to contact us at "}
+                    <Text.Anchor
+                      href="mailto:mammaritascucina@gmail.com"
+                    >
+                      mammaritascucina@gmail.com
+                    </Text.Anchor>
+                    .
+                  </Text.SHeading>
+                </Container>
+              </div>
+            </div>
+          </Header.Overlay>
+        </Header.IndexWrapper>
+      </main>
+    </Page.Wrapper>
+  )
+}
