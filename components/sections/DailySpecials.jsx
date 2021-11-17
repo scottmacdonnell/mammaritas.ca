8c283d5461f5dda102d014aa62e8e9c61a1cdfe4
scottmacdonnell <scottsmacdonnell@gmail.com>
2021-11-17 00:20:35 -0500

    Update DailySpecials component

--- components/sections/DailySpecials.jsx
+++ components/sections/DailySpecials.jsx
@@ -21,45 +21,33 @@ export default function DailySpecials(props) {
               style={{ display: 'flex', flexDirection: 'column', gap: '0.75rem'}}
             >
               <MenuItem 
-                name="Bruschetta"
-                price="3pcs $7.95 / 5pcs $13.95"
-                description="Diced tomatoes mixed in an aglio olio herbed blend served on toasted bread."
+                name="Bacon, cheddar and cauliflower soup and a crostini"
+                price="$6.50"
+                description=""
               />
 
               <MenuItem
-                name="Garlic Mozzarella Bread"
-                price="3pcs $5.95 / 5pcs $9.95"
-                description="Crostini topped with garlic butter and fresh mozzarella."
+                name="Italian Meatloaf and a side house salad"
+                price="$10.50"
+                description=""
               />
 
               <MenuItem
-                name="Green olives"
-                price="$3.95"
-                description="Olives are coated with a spicy herb mixture according to olden day recipes.  Their flavour is unique, cheerfully spicy but delicate."
+                name="Delicious treats:"
+                price=""
+                description=""
               />
 
               <MenuItem
-                name="Homemade Meatballs"
-                price="3 pcs $10.95 / 5 pcs $14.95"
-                description="Ground beef, pork, Parmigiano Reggiano meatballs with house tomato sauce."
+                name="Pumpkin Tart or Apple Tart"
+                price="$3.50 each"
+                description=""
               />
 
               <MenuItem
-                name="Cacciatore Sausage"
-                price="$4.50"
-                description="Authentic Italian, your choice of either hot or mild."
-              />
-
-              <MenuItem 
-                name="Antipasto"
-                price="FOR 1 $9.95 / FOR 2 $14.95"
-                description="Prosciutto, salami, cheese, roasted peppers, olives, accompanied with crostini"
-              />
-
-              <MenuItem 
-                name="Insalata Caesar / Caesar Salad"
-                price="$7.25"
-                description="Romaine, bacon, freshly grated Parmigiano, oven-baked croutons and Caesar dressing."
+                name="Apple Tart"
+                price="$3.50 each"
+                description=""
               />
             </div>
           </div>
